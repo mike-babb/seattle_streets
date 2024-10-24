75beeeeea0e5767e99e167d7bae5fffc716232ba
Michael Babb <34557750+mike-babb@users.noreply.github.com>
2024-10-23 23:29:55 -0700

    remove duplicate code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # seattle_streets
+# October 19, 2024
+
+# Seattle's (disconnected) Street Network
+How many streets in Seattle are disconntinuous? A lot, it turns out.
+
+
+
+
---
+++ code/utils.py
@@ -0,0 +1,41 @@
+# mike babb
+# 2024 06 28
+# what streets start and stop?
+
+# standard
+import os
+
+# external
+from itertools import combinations, product
+import geopandas as gpd
+from matplotlib.patches import Rectangle
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from shapely.geometry import LineString, Point
+from shapely import line_merge
+
+
+
+
+def write_gdf(gdf: gpd.GeoDataFrame, output_file_path:str, output_file_name:str):
+    
+    ofpn = os.path.join(output_file_path, output_file_name)
+
+    if 'coords' in gdf.columns:
+        output_gdf = gdf.drop(labels = ['coords'], axis = 1)
+        output_gdf.to_file(filename = ofpn, driver = 'GPKG', index = False)
+    else:
+        gdf.to_file(filename = ofpn, driver = 'GPKG', index = False)
+
+    return None
+
+def subset_node_gdf(node_gdf:gpd.GeoDataFrame, other_node_df:pd.DataFrame):
+    node_subset_gdf = pd.merge(left = node_gdf, right = other_node_df)
+    return node_subset_gdf
+    
+
+if __name__ == '__main__':
+    print('find those streets')
