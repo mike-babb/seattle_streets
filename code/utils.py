4a04ae19f3077b2bd2aaf70db001b5a12913a566
Michael Babb <34557750+mike-babb@users.noreply.github.com>
2024-12-04 19:22:13 -0800

    add points2distance

--- code/utils.py
+++ code/utils.py
@@ -8,6 +8,7 @@ import os
 # external
 from itertools import combinations, product
 import geopandas as gpd
+import math
 from matplotlib.patches import Rectangle
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -34,6 +35,39 @@ def subset_node_gdf(node_gdf:gpd.GeoDataFrame, other_node_df:pd.DataFrame):
     node_subset_gdf = pd.merge(left = node_gdf, right = other_node_df)
     return node_subset_gdf
     
+
+
+def points2distance(start, end, unit):
+    """
+    Calculate distance (in kilometers) between two points given as (long, latt) pairs
+    based on Haversine formula (http://en.wikipedia.org/wiki/Haversine_formula).
+    Implementation inspired by JavaScript implementation from
+    http://www.movable-type.co.uk/scripts/latlong.html
+    Accepts coordinates as tuples (deg, min, sec), but coordinates can be given
+    in any form - e.g. can specify only minutes:
+    (0, 3133.9333, 0)
+    is interpreted as
+    (52.0, 13.0, 55.998000000008687)
+    """
+    # earths_radius_km = 6371 # kilometers
+    # earths_radius_miles = 3959 # miles
+    start_long = math.radians(start[0])
+    start_latt = math.radians(start[1])
+    end_long = math.radians(end[0])
+    end_latt = math.radians(end[1])
+    d_latt = end_latt - start_latt
+    d_long = end_long - start_long
+    a = math.sin(d_latt/2)**2 + math.cos(start_latt) * math.cos(end_latt) * math.sin(d_long/2)**2
+    c = 2 * math.atan2(math.sqrt(a),  math.sqrt(1-a))
+
+    if unit=="miles":
+        earths_radius = 3959
+    else:
+        earths_radius = 6731
+    # return the distance between the two points
+    return earths_radius * c
+
+
 
 if __name__ == '__main__':
     print('find those streets')
