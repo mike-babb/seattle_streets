083f2315102053fdd00b3c6309d0e86dbe7bb7b3
Michael Babb <34557750+mike-babb@users.noreply.github.com>
2025-02-07 16:58:44 -0800

    interim checkin 2025_02_07_16_58_44

--- code/map_and_data_copy.sh
+++ code/map_and_data_copy.sh
@@ -1,5 +1,5 @@
 # copy the map
-cp ../maps/discontinuous_Streets_v2.html /h/git/mike-babb.github.io/media/
+cp ../maps/discontinuous_Streets_vtwo.html /h/git/mike-babb.github.io/media/.
 
 # copy the data
-cp ../maps/all_streets_diss_v2.geojson /h/git/mike-babb.github.io/media/
+cp ../maps/all_streets_diss_v2.geojson /h/git/mike-babb.github.io/media/.
