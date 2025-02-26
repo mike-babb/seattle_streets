ecb77cb445e6faa025621e95279394b265d37406
Michael Babb <34557750+mike-babb@users.noreply.github.com>
2025-02-26 10:49:34 -0800

    interim checkin 2025_02_26_10_49_34

--- code/map_and_data_copy.sh
+++
@@ -1,8 +0,0 @@
-# copy the webmap
-cp ../maps/discontinuous_streets_v2.html /h/git/mike-babb.github.io/media/.
-
-# copy the geojson data
-cp ../maps/all_streets_diss_v2.geojson /h/git/mike-babb.github.io/media/.
-
-# copy the city sectors
-cp ../maps/city_sector_lines.geojson /h/git/mike-babb.github.io/media/.
